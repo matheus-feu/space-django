54ea121d5a43ad179e6553ba7744e89907e960c4
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-02-09 14:38:42 -0300

    fix

--- galeria/models.py
+++ galeria/models.py
@@ -1,13 +1,29 @@
+from datetime import datetime
 from django.db import models
+from stdimage.models import StdImageField
 
-
-# Create your models here.
 class Fotografia(models.Model):
-    titulo = models.CharField(max_length=100, blank=False, null=False)
-    tag = models.CharField(max_length=100, blank=False, null=False, default='tag')
-    legenda = models.CharField(max_length=150, blank=False, null=False)
-    descricao = models.TextField(max_length=500, blank=False, null=False)
-    imagem = models.ImageField(upload_to='fotos')
+    """Modelo de Fotografia, contém as informações de cada fotografia"""
+
+    OPCOES_CATEGORIA = [
+        ("Nebulosa", "NEBULOSA"),
+        ("Estrela", "ESTRELA"),
+        ("Galáxia", "GALÁXIA"),
+        ("Planeta", "PLANETA"),
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = StdImageField('Fotografia', upload_to='fotos/%Y/%m/%d/',
+                                variations={'thumbnail': {'width': 500, 'height': 500, 'crop': True}})
+    publicada = models.BooleanField(default=False)
+    data_publicacao = models.DateTimeField(default=datetime.now, blank=True)
+
+    class Meta:
+        verbose_name = 'Fotografia'
+        verbose_name_plural = 'Fotografias'
 
     def __str__(self):
-        return self.titulo, self.tag, self.legenda
+        return self.nome + " - " + self.categoria + " - " + self.legenda
--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
-# Create your views here.
+
 def index(request):
-    fotografias = Fotografia.objects.all()
-    return render(request, 'galeria/index.html', {'cards': fotografias})
+    """Função responsável pela exibição da página inicial, carrega todas as imagens publicadas na aplicação"""
+    fotografias = Fotografia.objects.filter(publicada=True).order_by('-id').all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    """Função que exibe uma imagem na página de detalhes, recebe as informações da imagem pelo id"""
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+
+def buscar(request):
+    """Busca por nome de fotografia e retorna as imagens encontradas na página de busca"""
+    fotografias = Fotografia.objects.filter(publicada=True).order_by('-id').all()
+
+    if "buscar" in request.GET:
+        termo = request.GET['buscar']
+        if termo:
+            fotografias = fotografias.filter(nome__icontains=termo)
+
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
